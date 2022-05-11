8b1bf948bd5f17746e6e0b4ea3d97cd1bdd5e1a3
Victor <vvsh1978@mail.ru>
2022-05-11 15:46:02 +0500

    feat: add showHero

--- src/modules/showHero.js
+++ src/modules/showHero.js
@@ -1,3 +1,37 @@
 export const showHero = (hero) => {
+    const heroBlock = document.querySelector('.hero-block')
+    
+    const heroSlide = document.createElement('div')
+    heroSlide.className = 'hero-slide swiper-slide'
+    
+    const heroImage = document.createElement('div')
+    heroImage.className = 'hero-image'
+    
+    const heroInfo = document.createElement('div')
+    heroInfo.className = 'hero-info'
 
+    for (let key in hero) {
+        if (key === 'photo') {
+            heroImage.innerHTML = `<img src=${'./' + hero[key]} alt="hero" class="hero-img">`
+        } else if (hero[key]) {
+            const heroInfoItem = document.createElement('div')
+            heroInfoItem.className = 'hero-info-item'
+            
+            const heroInfoItemTitle = document.createElement('div')
+            heroInfoItemTitle.className = 'hero-info-item-title'
+            heroInfoItemTitle.textContent = key
+            heroInfoItem.append(heroInfoItemTitle)
+
+            const heroInfoItemText = document.createElement('div')
+            heroInfoItemText.className = 'hero-info-item-text'
+            heroInfoItemText.textContent = hero[key]
+            heroInfoItem.append(heroInfoItemText)
+
+            heroInfo.append(heroInfoItem)
+        }
+    }
+
+    heroSlide.append(heroImage)
+    heroSlide.append(heroInfo)
+    heroBlock.append(heroSlide)
 }
